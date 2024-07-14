201f79f0aa57a93930d82d0fdaa56baf05f9b40a
Furkan Cengiz <muhammedcengiz1@gmail.com>
2024-07-14 00:30:23 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+	res.write("On the way to being a full snack engineer!");
+	res.end();
+});
+server.listen(3000);
+console.log("Server started on port 3000!");
